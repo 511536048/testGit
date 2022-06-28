8d2ff1f4edf55c83131de6abca3dbe58bb52504c
511536048 <511536048@qq.com>
2022-06-28 14:06:37 +0800

    增加注释

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,12 +1,39 @@
 package test;
-
+/**
+ * @ClassName: Test 
+ * @Description: 
+ * @author: shixinxin
+ * @date 2022年6月28日 下午1:53:38
+ */
 public class Test {
-
+	/**
+	 * 
+	 * @Title: main
+	 * @Description:
+	 * @param @param args
+	 * @return void
+	 * @throws
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("测试git");
 		System.out.println("提交远程库成功");
+		System.out.println("提交远程库成功2");
 
+	}
+	/**
+	 * 
+	 * @Description:
+	 * @author: shixinxin
+	 * @param @param a
+	 * @param @param b
+	 * @param @param str
+	 * @param @return
+	 * @return String
+	 * @throws
+	 * @date 2022年6月28日 下午2:05:45
+	 */
+	public static String check(int a,int b ,String str) {
+		return "test";
 	}
 
 }
